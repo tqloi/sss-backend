fc8b57060cf0c3cfe2add0d19ed94f221a95d3f2
CheeseMink <mjnhtran2004@gmail.com>
2026-01-23 22:07:15 +0700

    add API user-profile

---
+++ src/SSS.Web/Endpoints/Users/ChangePassword/ChangePasswordResponse.cs
@@ -0,0 +1,7 @@
+namespace SSS.WebApi.Endpoints.Users.ChangePassword;
+
+public sealed class ChangePasswordResponse
+{
+    public bool Success { get; set; }
+    public string Message { get; set; } = default!;
+}
