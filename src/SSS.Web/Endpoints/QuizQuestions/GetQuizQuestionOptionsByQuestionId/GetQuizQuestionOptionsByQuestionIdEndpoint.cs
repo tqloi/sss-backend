993469030a26b0b5fc977dac25474623af64e18a
thaiphuc1 <thaiphuc6104@gmail.com>
2026-03-04 21:50:37 +0700

    add getall quizquestion by quizid

---
+++ src/SSS.Web/Endpoints/QuizQuestions/GetQuizQuestionOptionsByQuestionId/GetQuizQuestionOptionsByQuestionIdEndpoint.cs
@@ -0,0 +1,23 @@
+﻿using FastEndpoints;
+using MediatR;
+using SSS.Application.Features.QuizQuestionOptions.GetQuizQuestionOptionsByQuestionId;
+
+namespace SSS.Web.Endpoints.QuizQuestions.GetQuizQuestionOptionsByQuestionId
+{
+    public class GetQuizQuestionOptionsByQuestionIdEndpoint(ISender sender)
+        : Endpoint<GetQuizQuestionOptionsByQuestionIdQuery, GetQuizQuestionOptionsByQuestionIdResult>
+    {
+        public override void Configure()
+        {
+            Get("/api/quiz-question/{questionId}/options");
+            Summary(s => s.Summary = "Get a quiz question by id");
+            Description(d => d.WithTags("QuizQuestions"));
+        }
+        public override async Task HandleAsync(GetQuizQuestionOptionsByQuestionIdQuery req, CancellationToken ct)
+        {
+            var result = await sender.Send(req, ct);
+            await SendAsync(result);
+        }
+
+    }
+}
