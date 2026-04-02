0485330de52f1896cec420e034e2b55490877700
tqloi <quyloivn@gmail.com>
2026-04-02 22:05:22 +0700

    refactor profile feature folders

--- src/SSS.Application/Features/UserProfile/GetUserMembership/GetUserMembershipQuery.cs
+++
@@ -1,9 +0,0 @@
-using MediatR;
-using SSS.Application.Features.UserProfile.Common;
-
-namespace SSS.Application.Features.UserProfile.GetUserMembership;
-
-public sealed class GetUserMembershipQuery : IRequest<UserMembershipDto>
-{
-    public string UserId { get; set; } = default!;
-}
