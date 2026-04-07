573bc06629fa224631f225af2b4017188710eb02
penguink9 <giangqt2k4@gmail.com>
2026-04-08 04:01:35 +0700

    Delete Not Used APIs

--- src/SSS.Application/Features/Content/RoadmapNodes/Delete/DeleteRoadmapNodeCommand.cs
+++
@@ -1,6 +0,0 @@
-using MediatR;
-
-namespace SSS.Application.Features.Content.RoadmapNodes.Delete
-{
-    public sealed record DeleteRoadmapNodeCommand(long RoadmapId, long NodeId) : IRequest<DeleteRoadmapNodeResult>;
-}
