5d4ff362ec53280f4ef3285ad77c15c948ba2328
tqloi <quyloivn@gmail.com>
2026-04-11 01:11:41 +0700

    add task permisson

--- src/SSS.Domain/Entities/Planning/TaskItem.cs
+++ src/SSS.Domain/Entities/Planning/TaskItem.cs
@@ -6,6 +6,7 @@ public class TaskItem
 {
     public long Id { get; set; }
     public long StudyPlanModuleId { get; set; }
+    public bool IsGenerateByAI { get; set; }
     public string Title { get; set; } = null!;
     public string? Description { get; set; }
     public Domain.Enums.TaskStatus? Status { get; set; }
